9a07faf73257ff36511c9f5638242283c5476e0c
Brian Arpaio <arpaio@gmail.com>
2016-03-14 12:03:05 -0400

    Finished Ch15 - Public Area

--- app/models/page.rb
+++ app/models/page.rb
@@ -13,7 +13,7 @@ class Page < ActiveRecord::Base
 	validates_presence_of :name 
 	validates_length_of :name, :maximum => 255
 	validates_presence_of :permalink
-	validates_length_of :permalink, :within => 3..255
+	# validates_length_of :permalink, :within => 3..255
 	validates_uniqueness_of :permalink
 
 	scope :visible, lambda {where(:visible => true)}
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
  
-  root "demo#index"
+  get 'public/index'
 
+  get 'public/show'
+
+  root "public#index"
+
+  get 'show/:permalink', :to => "public#show"
   get 'admin', :to => "access#index"
 
   #get 'demo/index'
